e26001e2ca70541d245115fec47da540786a2d08
Rakibul Hasan <129957520+rakib2606@users.noreply.github.com>
2023-04-06 13:19:09 +0600

    a program that takes two integer and display sum.

---
+++ SUM of two integer.c
@@ -0,0 +1,12 @@
+//Write a program that takes two integer and display sum
+#include<stdio.h>
+int main()
+{
+    int num1,num2,sum;
+    scanf("%d %d",&num1,&num2);
+
+    sum=num1+num2;
+    printf("The sum is: %d",sum);
+    return 0;
+
+}
