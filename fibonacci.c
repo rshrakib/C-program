888fe9f791df1ae9a1d0871461881c63ba5855f0
Rakibul Hasan <129957520+rakib2606@users.noreply.github.com>
2023-04-13 22:50:46 +0600

    Create Fibonacci seris

---
+++ fibonacci.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int first=0,second=1,count=0,fibo,n;
+    printf("Enter Your Range: ");
+    scanf("%d",&n);
+    while(count<n)
+    {
+        if(count<=1){
+                fibo=count;
+        }
+
+            else
+        {
+            fibo=first+second;
+        first=second;
+        second=fibo;
+        }
+
+        printf("%d ",fibo);
+        count++;
+    }
+
+    return 0;
+
+
+}
