e39dcf8d477e63d278c1c4df2b3d60653a0247e1
Rakibul Hasan <129957520+rakib2606@users.noreply.github.com>
2023-05-05 00:20:10 +0600

    Create bee 1049.c

---
+++ bee 1049.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+int main()
+{
+    char a[40];
+    char b[40];
+    char c[40];
+    gets(a);
+    gets(b);
+    gets(c);
+if(a[0]=='v' && b[0]=='a' && c[0]=='c')
+    {
+        printf("aguia\n");
+    }
+if(a[0]=='v' && b[0]=='a' && c[0]=='o')
+    {
+        printf("pomba\n");
+    }
+if(a[0]=='v' && b[0]=='m' && c[0]=='o')
+    {
+        printf("homem\n");
+    }
+if(a[0]=='v' && b[0]=='m' && c[0]=='h')
+    {
+        printf("vaca\n");
+    }
+if(a[0]=='i' && b[0]=='i' && c[2]=='m')
+    {
+        printf("pulga\n");
+    }
+if(a[0]=='i' && b[0]=='i' && c[2]=='r')
+    {
+        printf("lagarta\n");
+    }
+
+if(a[0]=='i' && b[0]=='a' && c[0]=='h')
+    {
+        printf("sanguessuga\n");
+    }
+if(a[0]=='i' && b[0]=='a' && c[0]=='o')
+    {
+        printf("minhoca\n");
+    }
+return 0;
+
+}
